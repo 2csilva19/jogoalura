7984986325eb35fc06d2bbadd7005b473b2489cc
2csilva19 <00001202907258sp@al.educacao.sp.gov.br>
2024-09-30 15:27:41 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Gabriel silva 2C*/
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
